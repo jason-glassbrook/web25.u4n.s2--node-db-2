bb9427f0acb81395dfd89bf4e98a29860c73199a
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-21 16:31:06 -0500

    fill out schema for 'cars'

--- data/migrations/20200121161019_cars.js
+++ data/migrations/20200121161019_cars.js
@@ -1,7 +1,37 @@
 
 exports.up = (knex) => (
   knex.schema.createTable ('cars', (table) => {
-
+    table
+      .timestamps (true, true)
+    table
+      .uuid ('id')
+      .primary ()
+    table
+      .string ('vin', 255)
+      .unique ()
+      .notNullable ()
+    table
+      .string ('make', 255)
+      .notNullable ()
+    table
+      .string ('model', 255)
+      .notNullable ()
+    table
+      .float ('distance_driven_value')
+      .notNullable ()
+    table
+      .string ('distance_driven_scale')
+      .defaultTo ('miles')
+      .notNullable ()
+    table
+      .enum ('transmission_type', [
+        'automatic',
+        'manual',
+      ])
+      .nullable ()
+    table
+      .string ('title_status', 255)
+      .nullable ()
   })
 )
 

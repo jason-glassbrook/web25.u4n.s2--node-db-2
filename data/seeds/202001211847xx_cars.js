c08ddbb1656a69e41bacaf3c5ea23271169e2fd2
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-21 19:06:24 -0500

    write seed for 'cars'

--- data/seeds/202001211847xx_cars.js
+++ data/seeds/202001211847xx_cars.js
@@ -1,13 +1,45 @@
+const name = 'cars'
 
-exports.seed = function(knex) {
+exports.seed = (knex) => (
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  knex (name)
+  .truncate ()
+  .then (() => {
+    // Inserts seed entries
+    return (
+      knex (name)
+      .insert ([
+        {
+          'id' : 1,
+          'vin' : 'd7a17051-9b10-4a22-8966-739f5d80f824',
+          'make' : 'Subaru',
+          'model' : 'Impreza',
+          'year' : 2014,
+          'distance_driven' : 45309,
+          'distance_driven_scale' : 'miles',
+          'transmission' : 'automatic',
+        },
+        {
+          'id' : 2,
+          'vin' : '71070bfb-0d85-46a3-8c27-beab217cbc24',
+          'make' : 'Honda',
+          'model' : 'Civic',
+          'year' : 2000,
+          'distance_driven' : 225311,
+          'distance_driven_scale' : 'kilometers',
+          'transmission' : 'automatic',
+        },
+        {
+          'id' : 3,
+          'vin' : '11b36bc6-8773-4a1c-bcd7-b644ef568f29',
+          'make' : 'Toyota',
+          'model' : 'Previa',
+          'year' : 1993,
+          'distance_driven' : 265149873,
+          'distance_driven_scale' : 'meters',
+          'transmission' : 'manual',
+        },
+      ])
+    )
+  })
+)
